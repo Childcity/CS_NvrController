db972172a9403fe0d3a2ca508eab6b487c5a9944
Childcity <skulazkiy@gmail.com>
2019-12-08 01:49:37 +0200

    make preview as separate window

---
+++ CS_NVRControllerGUI/GUI/PreviewWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_NVRControllerGUI.GUI {
+	public partial class PreviewWindow: Form {
+		public PreviewWindow()
+		{
+			InitializeComponent();
+		}
+
+		public IntPtr getLiveViewHandle() => streamWnd1.Handle;
+
+		public void resetLiveView()
+		{
+			streamWnd1.BackColor = Color.White;
+			streamWnd1.BackColor = Color.Gainsboro;
+		}
+	}
+}
